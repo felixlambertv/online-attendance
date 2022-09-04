94b418de0e0140be2e267680ccf5ae465b27e42b
Felix Lambert <felix@itsavirus.com>
2022-09-05 00:04:47 +0800

    Add error handler

--- config/server.go
+++ config/server.go
@@ -2,6 +2,10 @@ package config
 
 import (
 	"fmt"
+	"github.com/felixlambertv/online-attendance/controller"
+	"github.com/felixlambertv/online-attendance/middleware"
+	"github.com/felixlambertv/online-attendance/repository"
+	"github.com/felixlambertv/online-attendance/service"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -11,16 +15,26 @@ import (
 func SetupServer(db *gorm.DB) *gin.Engine {
 	appConfig := GetConfig()
 	engine := gin.Default()
+	engine.Use(gin.Recovery())
+	engine.Use(middleware.ErrorHandler)
 
-	apiGroup := engine.Group("api")
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository)
+	userController := controller.NewUserController(userService)
+
+	v1Routes := engine.Group("api/v1")
 	{
-		v1Routes := apiGroup.Group("/v1")
+		v1Routes.GET("/ping", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{"pong": appConfig.AppName})
+		})
+
+		userRoutes := v1Routes.Group("/users")
 		{
-			v1Routes.GET("/ping", func(context *gin.Context) {
-				context.JSON(http.StatusOK, gin.H{"ping": appConfig.AppName})
-			})
+			userRoutes.POST("/", userController.Register)
+			userRoutes.GET("/:userId", userController.Detail)
 		}
 	}
+
 	err := engine.Run(fmt.Sprintf(":%s", appConfig.Server.Port))
 	if err != nil {
 		logrus.Error("fail run app engine")
